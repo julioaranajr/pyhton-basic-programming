43cf70d6805ac22ae27e5bcdecf67f64c6327c2c
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-07 08:54:13 +0200

    python app examples
    Talent Academy - Hands on Labs

---
+++ inputs.py
@@ -0,0 +1,6 @@
+apples = input("How many apples do you have ?")
+price = 0.415
+print("You have " + apples + " apples!")
+
+total = float(apples) * price
+print("Total price in Euros is = " + str(total))
