7c60a701e2c8d4519d6eb1ba9031545fb765325f
Julio Arana Jr <113112001+julioaranajr@users.noreply.github.com>
2023-03-18 14:20:02 +0100

    Update variables.py

--- apps examples/variables.py
+++ apps examples/variables.py
@@ -3,7 +3,7 @@ apples = 12            # type integer
 price = 4.99           # type float
 is_fruit = True         # type boolean
 variety_name = "Pink Lady"    # type String
-print = (fruit)
+print(fruit)
 print(apples)
 print(price)
 print(is_fruit)
